907a677c499b72396e00f0bac0f4b5ea619ee68a
matitorres <mati.torrest@duocuc.cl>
2025-12-02 20:08:44 -0300

    2do procfile

--- cevicheria/asgi.py
+++ cevicheria/asgi.py
@@ -2,19 +2,15 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import django
+import pedidos.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cevicheria.settings')
-
-# 👇 Esto es CLAVE para evitar el error
-django.setup()
-
-# Solo después de setup() podemos importar routing
-from pedidos.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
+        URLRouter(
+            pedidos.routing.websocket_urlpatterns
+        )
     ),
 })
